8b9d884b77e0a7354c19699e0337ccfaa9391582
AxelOffal <aj@stillhq.com>
2023-08-30 12:51:03 +1000

    Dynamic Scraping Update
    replaced beautiful soup for selenium and started working on different dynamic sites

--- scraper.py
+++ scraper.py
@@ -1,32 +1,20 @@
-import requests
-from bs4 import BeautifulSoup
-#pretty print is used mostly to format results and make reading through html content easier and clearer
-from pprint import pprint
-#testing requests_html, not working just yet
-from requests_html import HTMLSession
+from selenium import webdriver
+import time
 
-#not working method
-#gets the html information of a dynamic site
-#method takes the link and header information used to represent a browser when requesting links, by default it is set to represent a firefox user
-def getHtmlSession(link, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}):
-    session = HTMLSession()
-    #request for the sites html information from our given link
-    data = session.get(link)
-    data.html.render(sleep=5)
+options = webdriver.ChromeOptions()
+#options.headless = True
 
-    html = data.html.text
-    #html = BeautifulSoup(data.html.html, 'html.parser')
-    
-    return html
+driver = webdriver.Chrome(options=options)
 
-#gets the html information of a link
-#method takes the link and header information used to represent a browser when requesting links, by default it is set to represent a firefox user
-def getHtml(link, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}):
-    #request for the sites html information from our given link
-    data = requests.get(link, headers=headers, stream=True, timeout=10).content
-    #pass our html information using BeautifulSoup so that it can be filtered through
-    html = BeautifulSoup(data, 'html.parser')
-    return html
+def getHTML(url):
+    #get the html information from the inputed url and load it with chrome
+    driver.get(url)
+    #wait for a small amount of time to let the site load
+    time.sleep(10)
+    #exit chrome
+    driver.quit
+    #return the driver data
+    return driver
 
 #takes a json object result as a string and turns it back into its respective dictionary/list for use
 def interpretJson(json):
