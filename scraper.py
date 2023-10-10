18b43ec3804245a9cad3778a14a15ed03321161d
AxelOffal <aj@stillhq.com>
2023-10-10 13:56:54 +1100

    Set headless options
    Set the headless driver as true and the default. Added alternative method to turn on headed driver for some sites

--- scraper.py
+++ scraper.py
@@ -3,17 +3,27 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 import time
 
+#get the options for the chrome driver
 options = webdriver.ChromeOptions()
-#options.headless = True
-DRIVER_PATH = "C:\\Users\\aj\\OneDrive\\code\\year 3\\IT Project2\\driver\\chromedriver_win32\\chromedriver"
+#get the headless value and set it to True
+options.headless = True
+#define the driver as a chrome webdriver with the appropriate options and the service set as a chrome driver.
 driver = webdriver.Chrome(options=options,service=Service(ChromeDriverManager().install()))
+#if there is not an appropriate chrome driver defined for the driver above, the
+#Service(ChromeDriverManager().install()) method will create one for use
 
-def getHTML(url):
+def getHTML(url, headless = True):
+    #if headless is turned off redefine the driver with the headless feature turned off
+    if not headless:
+        options = webdriver.ChromeOptions()
+        options.headless = False
+        driver = webdriver.Chrome(options=options,service=Service(ChromeDriverManager().install()))
+    
     #get the html information from the inputed url and load it with chrome
     driver.get(url)
     #wait for a small amount of time to let the site load
     driver.implicitly_wait(0.5)
-    time.sleep(4)
+    time.sleep(5)
     #exit chrome
     driver.quit
     #return the driver data
