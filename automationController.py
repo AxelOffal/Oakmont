c0462a3d3d236226955aabff41988c6e8807525a
Ryan Leach <116049103+RLeach02@users.noreply.github.com>
2023-10-20 09:03:16 +1100

    Automation and Integration
    Adaptations have been made to the web scraping and prediction code files to facilitate returning of data variable values. The automation controller is built and integrated, connecting to the database to process flow of information and data storage/sanitisation and processing.

---
+++ automationController.py
@@ -0,0 +1,321 @@
+import mysql.connector
+from mysql.connector import errorcode
+import requests
+
+from getInflation import (
+    getRBAInflation,
+    getExpantismSectorPrices,
+    getExpantismMonthlyCost,
+    getColesApples,
+    getColesChicken,
+    getColesBread,
+    getColesBananas,
+)
+from getInflation import (
+    getWooliesBread,
+    getWooliesApples,
+    getWooliesBananas,
+    getWooliesChicken,
+)
+from getInflation import getAIPFuel, getCoreLogic, getAlcoholDaniels, getAlcoholAsahi
+
+
+def create_db_connection():
+    try:
+        connection = mysql.connector.connect(
+            host="inflationdb.mysql.database.azure.com",
+            username="Oakmont",
+            password="StrattonStonks741",
+            database="oakmont_padb",
+        )
+        print("Connection established\n")
+        return connection
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Error: Access denied. Check your credentials.")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Error: Database does not exist.")
+        else:
+            print(f"Error: {err}")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def update_db(name, sku, description, brand_name, url, expenditure_class, price):
+    try:
+        cursor = connection.cursor()
+        insert_product = "INSERT INTO products (names, sku, description, brand_name, url, expenditure_class) VALUES (%s, %s, %s, %s, %s, %s)"
+        cursor.execute(
+            insert_product, (name, sku, description, brand_name, url, expenditure_class)
+        )
+        connection.commit()
+
+        product_id = cursor.lastrowid
+        insert_price = "INSERT INTO prices (productID, price) VALUES (%s, %s)"
+        cursor.execute(insert_price, (product_id, price))
+        connection.commit()
+        print("Data successfully inserted into the database.")
+    except mysql.connector.Error as err:
+        print(f"MySQL error: {err}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        cursor.close()
+
+
+# Call the functions and print the results
+if __name__ == "__main__":
+    try:
+        print(":::Scraping Beginning:::\n")
+
+        connection = create_db_connection()
+
+        print("Getting RBA inflation data...")
+        CPI, MPI = getRBAInflation()
+        url = "https://www.rba.gov.au/inflation-overview.html"
+        print(f"Consumer Price Index: {CPI}")
+        print(f"Monthly Price Index: {MPI}")
+
+        print("\nGetting sector prices from expatistan...")
+        url = "https://www.expatistan.com/cost-of-living/country/australia"
+        sector_prices = getExpantismSectorPrices()
+        for item in sector_prices:
+            name = item[0]
+            price = item[1]
+            url = url
+
+            print(f"Item: {item[0]}, Price: {item[1]}")
+
+        print("\nGetting monthly cost from expatistan...")
+        url = "https://www.expatistan.com/cost-of-living/country/australia"
+        monthly_cost = getExpantismMonthlyCost()
+        print(f"Monthly Cost: {monthly_cost}")
+
+        print("\nGetting Coles apples price...")
+        apples_price, apples_url = getColesApples()
+        name = "Apples"
+        sku = ""
+        description = apples_price[1]
+        brand_name = "Coles"
+        url = apples_url
+        expenditure_class = "Food"
+
+        price = apples_price[0]
+
+        update_db(name, sku, description, brand_name, url, expenditure_class, price)
+
+        print(
+            f"Coles Apples Price: {apples_price[0]}, Weight: {apples_price[1]}, URL: {apples_url}"
+        )
+
+        print("\nGetting Coles bread price...")
+        bread_price, bread_url = getColesBread()
+        name = "Bread"
+        sku = ""
+        description = bread_price[1]
+        brand_name = "Coles"
+        url = bread_url
+        expenditure_class = "Food"
+
+        price = bread_price[0]
+
+        update_db(name, sku, description, brand_name, url, expenditure_class, price)
+
+        print(
+            f"Coles Bread Price: {bread_price[0]}, Weight: {bread_price[1]}, URL: {bread_url}"
+        )
+
+        print("\nGetting Coles bananas price...")
+        bananas_price, bananas_url = getColesBananas()
+        name = "Bananas"
+        sku = ""
+        description = bananas_price[1]
+        brand_name = "Coles"
+        url = bananas_url
+        expenditure_class = "Food"
+
+        price = bananas_price[0]
+
+        update_db(name, sku, description, brand_name, url, expenditure_class, price)
+
+        print(
+            f"Coles Bananas Price: {bananas_price[0]}, Weight: {bananas_price[1]}, URL: {bananas_url}"
+        )
+
+        print("\nGetting Coles chicken price...")
+        chicken_price, chicken_url = getColesChicken()
+        name = "Chicken"
+        sku = ""
+        description = chicken_price[1]
+        brand_name = "Coles"
+        url = chicken_url
+        expenditure_class = "Food"
+
+        price = chicken_price[0]
+
+        update_db(name, sku, description, brand_name, url, expenditure_class, price)
+
+        print(
+            f"Coles Chicken Price: {chicken_price[0]}, Weight: {chicken_price[1]}, URL: {chicken_url}"
+        )
+
+        print("\nGetting Woolies bread price...")
+        woolies_bread_price, woolies_bread_url = getWooliesBread()
+        name = "Bread"
+        sku = ""
+        description = woolies_bread_price[1]
+        brand_name = "Woolworths"
+        url = woolies_bread_url
+        expenditure_class = "Food"
+
+        price = woolies_bread_price[0]
+
+        update_db(name, sku, description, brand_name, url, expenditure_class, price)
+
+        print(
+            f"Woolies Bread Price: {woolies_bread_price[0]}, Weight: {woolies_bread_price[1]}, URL: {woolies_bread_url}"
+        )
+
+        print("\nGetting Woolies apples price...")
+        woolies_apples_price, woolies_apples_url = getWooliesApples()
+        name = "Apples"
+        sku = ""
+        description = woolies_apples_price[1]
+        brand_name = "Woolworths"
+        url = woolies_apples_url
+        expenditure_class = "Food"
+
+        price = woolies_apples_price[0]
+
+        update_db(name, sku, description, brand_name, url, expenditure_class, price)
+
+        print(
+            f"Woolies Apples Price: {woolies_apples_price[0]}, Weight: {woolies_apples_price[1]}, URL: {woolies_apples_url}"
+        )
+
+        print("\nGetting Woolies bananas price...")
+        woolies_bananas_price, woolies_bananas_url = getWooliesBananas()
+        name = "Bananas"
+        sku = ""
+        description = woolies_bananas_price[1]
+        brand_name = "Woolworths"
+        url = woolies_bananas_url
+        expenditure_class = "Food"
+
+        price = woolies_bananas_price[0]
+
+        update_db(name, sku, description, brand_name, url, expenditure_class, price)
+
+        print(
+            f"Woolies Bananas Price: {woolies_bananas_price[0]}, Weight: {woolies_bananas_price[1]}, URL: {woolies_bananas_url}"
+        )
+        """
+        print("\nGetting Woolies chicken price...")
+        woolies_chicken_price, woolies_chicken_url = getWooliesChicken()
+        name = "Chicken"
+        sku = ""
+        description = woolies_chicken_price[1]
+        brand_name = "Woolworths"
+        url = woolies_chicken_url
+        expenditure_class = "Food"
+
+        price = woolies_chicken_price[0]
+
+        update_db(name, sku, description, brand_name, url, expenditure_class, price)
+
+        print(
+            f"Woolies Chicken Price: {woolies_chicken_price[0]}, Weight: {woolies_chicken_price[1]}, URL: {woolies_chicken_url}"
+        )
+        """
+        print("\nGetting AIP fuel price...")
+        url = "http://www.aip.com.au/pricing/national-retail-petrol-prices"
+        aip_fuel_price = getAIPFuel()
+        name = "Fuel"
+        sku = ""
+        description = "AIP Fuel Price"
+        brand_name = "AIP"
+        url = url
+        expenditure_class = "Transport"
+
+        price = aip_fuel_price
+
+        update_db(name, sku, description, brand_name, url, expenditure_class, price)
+
+        print(f"AIP Fuel Price: {aip_fuel_price}")
+
+        print("\nGetting CoreLogic housing prices...")
+        url = "https://www.corelogic.com.au/our-data/corelogic-indices"
+        corelogic_prices = getCoreLogic()
+        for item in corelogic_prices:
+            name = f"{item[0]} Housing Price"
+            sku = ""
+            description = "Housing Price"
+            brand_name = "CoreLogic"
+            url = url
+            expenditure_class = "Housing"
+
+            price = item[1]
+
+            update_db(name, sku, description, brand_name, url, expenditure_class, price)
+
+            print(f"Location: {item[0]}, Price Index: {item[1]}")
+
+        print("\nGetting Jack Daniels price...")
+        url = "https://www.mybottleshop.au/jack-daniels-1907-white-label-heritage-bottle-700ml"
+        jack_daniels_price = getAlcoholDaniels()
+        name = "Jack Daniels"
+        sku = ""
+        description = "Jack Daniels Alcohol Price"
+        brand_name = "mybottleshop.au"
+        url = url
+        expenditure_class = "Alcohol"
+
+        price = jack_daniels_price
+
+        update_db(name, sku, description, brand_name, url, expenditure_class, price)
+
+        print(f"Jack Daniels Price: {jack_daniels_price}")
+
+        print("\nGetting Asahi price...")
+        url = (
+            "https://www.mybottleshop.au/asahi-super-dry-black-bottle-18x334ml-bottles"
+        )
+        asahi_price = getAlcoholAsahi()
+        name = "Asahi"
+        sku = ""
+        description = "Asahi Alcohol Price"
+        brand_name = "mybottleshop.au"
+        url = url
+        expenditure_class = "Alcohol"
+
+        price = asahi_price
+
+        update_db(name, sku, description, brand_name, url, expenditure_class, price)
+
+        print(f"Asahi Price: {asahi_price}")
+
+        print("\n:::Scraping Complete:::\n")
+
+        print(":::Calculating Inflation:::\n")
+        import inflationCalc
+
+        final_estimation = inflationCalc.return_inflation()
+        print(f"Final Estimation: {final_estimation}")
+        final_estimation = final_estimation.replace("%", "").strip()
+        try:
+            cursor = connection.cursor()
+            insert_result = "INSERT INTO results (inflation_prediction) VALUES (%s)"
+            cursor.execute(insert_result, (final_estimation,))
+            connection.commit()
+            print("Inflation calculated and inserted into the database.")
+        except mysql.connector.Error as err:
+            print(f"MySQL error: {err}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            cursor.close()
+
+        connection.close()
+    except Exception as e:
+        print(f"An error occurred: {e}")
--- inflationCalc.py
+++ inflationCalc.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 3 21:21:50 2023
-
-@author: willi
-"""
 import mysql.connector
 from datetime import datetime, timedelta
 
-cnx = mysql.connector.connect(user='Oakmont', password='StrattonStonks741',
-                              host='inflationdb.mysql.database.azure.com',
-                              database='oakmont_padb')
+cnx = mysql.connector.connect(
+    user="Oakmont",
+    password="StrattonStonks741",
+    host="inflationdb.mysql.database.azure.com",
+    database="oakmont_padb",
+)
 
 cursor = cnx.cursor()
 
@@ -19,58 +17,76 @@ cursor.execute(query_classes)
 all_classes = cursor.fetchall()
 cursor.close()  # Close the cursor after fetching the results
 
-class_inflation_list = []  
+class_inflation_list = []
 
 # Iterate through each class
 for e_class in all_classes:
-    e_class = e_class[0]  
+    e_class = e_class[0]
     print(f"Processing products of class: {e_class}")
-    
+
     inflation_sum = 0
     product_count = 0
-    
+
     cursor = cnx.cursor()  # Create a new cursor for this loop
-    query_products = ("SELECT productID, names FROM products "
-                      "WHERE expenditure_class = %s")
+    query_products = (
+        "SELECT productID, names FROM products " "WHERE expenditure_class = %s"
+    )
     cursor.execute(query_products, (e_class,))
     products = cursor.fetchall()
     cursor.close()  # Close the cursor after fetching the results
-    
+
     for productID, names in products:
         cursor = cnx.cursor()  # Create a new cursor for this loop
+
         # Get the most recent price
-        query_latest_price = ("SELECT price FROM prices "
-                              "WHERE productID = %s "
-                              "ORDER BY timestamp DESC LIMIT 1")
+        query_latest_price = (
+            "SELECT price FROM prices "
+            "WHERE productID = %s "
+            "ORDER BY timestamp DESC LIMIT 1"
+        )
         cursor.execute(query_latest_price, (productID,))
-        latest_price = cursor.fetchone()[0]
+        latest_price_result = cursor.fetchone()
 
         # Get a past price
-        query_past_price = ("SELECT price FROM prices "
-                            "WHERE productID = %s "
-                            "ORDER BY timestamp ASC LIMIT 1")
+        query_past_price = (
+            "SELECT price FROM prices "
+            "WHERE productID = %s "
+            "ORDER BY timestamp ASC LIMIT 1"
+        )
         cursor.execute(query_past_price, (productID,))
-        past_price = cursor.fetchone()[0]
+        past_price_result = cursor.fetchone()
+
         cursor.close()  # Close the cursor after fetching the results
 
-        # Calculate the inflation
-        if past_price != 0:  # Add a check to avoid division by zero
-            inflation = (latest_price - past_price) / past_price
-            print(f"Inflation for product {productID} ({names}):({latest_price} - {past_price}) / {past_price} = {inflation * 100:.2f}%")
-            inflation_sum += inflation
-            product_count += 1
+        # Check if both latest and past prices are found
+        if latest_price_result is not None and past_price_result is not None:
+            latest_price = latest_price_result[0]
+            past_price = past_price_result[0]
+
+            # Calculate the inflation
+            if past_price != 0:  # Add a check to avoid division by zero
+                inflation = (latest_price - past_price) / past_price
+                print(
+                    f"Inflation for product {productID} ({names}):({latest_price} - {past_price}) / {past_price} = {inflation * 100:.2f}%"
+                )
+                inflation_sum += inflation
+                product_count += 1
+            else:
+                print("Past price is zero, cannot calculate inflation.")
         else:
-            print("Past price is zero, cannot calculate inflation.")
-            
+            print(f"No price data found for product {productID} ({names}).")
+
     if product_count > 0:
         avg_inflation = inflation_sum / product_count
         print(f"Average inflation for class {e_class}: {avg_inflation * 100:.2f}%")
-        class_inflation_list.append((e_class, avg_inflation))  # Adding the class and avg inflation to the list
+        class_inflation_list.append(
+            (e_class, avg_inflation)
+        )  # Adding the class and avg inflation to the list
     else:
         print(f"No valid inflation data available for class {e_class}.")
 for item in class_inflation_list:
     print(f"Class: {item[0]}, Average Inflation: {item[1] * 100:.2f}%")
-    
+
 # Retrieve the weights for the classes that we have data for
 cursor = cnx.cursor()
 
@@ -78,11 +94,10 @@ cursor = cnx.cursor()
 class_weights = []
 
 for e_class, _ in class_inflation_list:
-    query_weights = ("SELECT weights FROM abs_weights "
-                     "WHERE expenditure_class = %s")
+    query_weights = "SELECT weights FROM abs_weights " "WHERE expenditure_class = %s"
     cursor.execute(query_weights, (e_class,))
     weight = cursor.fetchone()
-    
+
     if weight is not None:  # Check if weight is found for the class
         class_weights.append((e_class, weight[0]))
 
@@ -92,7 +107,9 @@ cursor.close()
 total_weight = sum(weight for _, weight in class_weights)
 
 # Rescale the weights so they add up to 1
-rescaled_weights = [(e_class, (weight / total_weight)/100) for e_class, weight in class_weights]
+rescaled_weights = [
+    (e_class, (weight / total_weight) / 100) for e_class, weight in class_weights
+]
 
 # Print out the rescaled weights for verification
 for e_class, weight in rescaled_weights:
@@ -101,8 +118,8 @@ for e_class, weight in rescaled_weights:
 weighted_inflations = []
 
 # Multiply each rescaled weight by the average inflation in each class
-for (e_class, avg_inflation) in class_inflation_list:
-    for (class_with_weight, rescaled_weight) in rescaled_weights:
+for e_class, avg_inflation in class_inflation_list:
+    for class_with_weight, rescaled_weight in rescaled_weights:
         if e_class == class_with_weight:
             weighted_inflation = float(avg_inflation) * rescaled_weight
             weighted_inflations.append((e_class, weighted_inflation))
@@ -116,15 +133,16 @@ print(f"Overall Inflation: {overall_inflation:.6f}")
 cursor = cnx.cursor()
 
 # Getting the latest CPI value
-query_latest_cpi = ("SELECT date, value FROM abs_inflation "
-                    "ORDER BY date DESC LIMIT 1")
+query_latest_cpi = "SELECT date, value FROM abs_inflation " "ORDER BY date DESC LIMIT 1"
 cursor.execute(query_latest_cpi)
 latest_cpi_date, latest_cpi_value = cursor.fetchone()
 print(f"Latest CPI value: {latest_cpi_value} at {latest_cpi_date}")
 
 # Getting the CPI value from 11 months ago
-query_past_cpi = (f"SELECT date, value FROM abs_inflation "
-                  f"WHERE date = '{latest_cpi_date}' - INTERVAL 11 MONTH")
+query_past_cpi = (
+    f"SELECT date, value FROM abs_inflation "
+    f"WHERE date = '{latest_cpi_date}' - INTERVAL 11 MONTH"
+)
 cursor.execute(query_past_cpi)
 past_cpi_date, past_cpi_value = cursor.fetchone()
 print(f"CPI value from 11 months ago: {past_cpi_value} at {past_cpi_date}")
@@ -135,7 +153,12 @@ if past_cpi_value != 0:  # Add a check to avoid division by zero
     print(f"CPI Inflation over the past 11 months: {cpi_inflation * 100:.2f}%")
 else:
     print("Past CPI value is zero, cannot calculate inflation.")
-final_inflation = cpi_inflation + (overall_inflation*4)
+final_inflation = cpi_inflation + (overall_inflation * 4)
 print(f"Final estimation = {final_inflation*100:.5F}%")
+returnsInflationFinal = (f"{final_inflation*100:.5F}%")
+
 cursor.close()
 cnx.close()
+
+def return_inflation():
+    return returnsInflationFinal
